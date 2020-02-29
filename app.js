6e8d888bbf77cec78b9a46162b9bf89cee0e6ca6
StellaYangF <1062878468@qq.com>
2020-02-29 17:32:11 +0800

    express.static added

--- app.js
+++ app.js
@@ -1,30 +1,9 @@
 const express = require('express');
 const app = express();
-
-console.log('111');
-
-
+const path = require('path');
 const Router = express.Router;
-express.static = function (dirname) {
-    return (req, res, next) => {
-        let path = require('path');
-        let fs = require('fs');
-        let absPath = path.join(dirname, req.path);
-        console.log(absPath)
-        fs.stat(absPath, function (err, statObj) {
-            if (err) {
-                return next();
-            }
-            if (statObj.isFile()) {
-                res.sendFile(absPath);
-            }else{
-            //   找index.html ...
-            }
-        })
-    }
-}
 
-app.use(express.static("./public"));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 let user = new Router();
 user.get('/remove', (req, res, next) => {
--- express/lib/express.js
+++ express/lib/express.js
@@ -1,9 +1,27 @@
 const Application = require('./application');
 const Router = require('./router');
 
+
 function createApplication() {
     return new Application();
 }
 createApplication.Router = Router;
+
+createApplication.static = function (dirname) {
+    return (req, res, next) => {
+        let path = require('path');
+        let fs = require('fs');
+        let absPath = path.join(dirname, req.path);
+        fs.stat(absPath, function (err, statObj) {
+            if (err) {
+                return next();
+            }
+            if (!statObj.isFile()) {
+                absPath = absPath + '/index.html';
+            } 
+            res.sendFile(absPath);
+        })
+    }
+}
 
 module.exports = createApplication;
